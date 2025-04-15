e73921bd7ff61e50e17f9ae01adbce14266da0e1
Diwaker kanojia <diwakerkanojia17@gmail.com>
2025-04-16 00:13:14 +0530

    Fixing error

--- src/Components/Common/Button/Button.jsx
+++ src/Components/Common/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button = ({ text, onClick, outline }) => {
+const Button = ({ text, outline }) => {
   return (
     <div
       className={
@@ -8,7 +8,6 @@ const Button = ({ text, onClick, outline }) => {
           ? "bg-transparent text-black border-2 border-blue py-2 px-6 rounded-3xl text-center font-semibold cursor-pointer min-w-[130px] hover:bg-blue transition-all duration-200 capitalize dark:text-white"
           : `bg-blue text-white py-2 px-6 border-2 border-blue rounded-3xl font-semibold text-center cursor-pointer shadow-lg hover:shadow-drop transition-all duration-200 min-w-[100px] capitalize`
       }
-      onClick={() => onClick()}
     >
       {text}
     </div>
